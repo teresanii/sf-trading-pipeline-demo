e261a2846c6f577c82d42cb86e9fba7e1a23f260
teresanii <teresa.raquel@gmail.com>
2025-07-04 00:20:37 +0200

    fixed api integration creation

--- 0_create_sf_git_integration.sql
+++ 0_create_sf_git_integration.sql
@@ -1,13 +1,9 @@
-/***************************************************************************************************          
-Trading data analytichands on lab
-Creates database, schemas, stages, file formats, and tables for the data pipeline
-Run this script once to set up your Snowflake environment
-****************************************************************************************************/
+USE ROLE ACCOUNTADMIN;
 
 -- Create an API integration so Snowflake can access a github repository
 CREATE OR REPLACE API INTEGRATION GITHUB_INTEGRATION
   API_PROVIDER = git_https_api
-  API_ALLOWED_HOSTS = ('https://github.com')
+  API_ALLOWED_PREFIXES = ('https://github.com')
   ENABLED = TRUE;
 
 -- Check if integration
